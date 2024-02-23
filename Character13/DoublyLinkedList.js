e107d2c36e644f48a5b1ce374fd3dde9b159de90
montoyitadevelp <simon1018226487@gmail.com>
2024-02-23 07:31:21 -0500

    feat: Exercises LinkedList

--- Character13/DoublyLinkedList.js
+++ Character13/DoublyLinkedList.js
@@ -46,9 +46,81 @@ DoublyLinkedList.prototype.insertAtTail = function (value) {
   this.size++;
 };
 
+DoublyLinkedList.prototype.deleteAtHead = function () {
+  let toReturn = null;
+
+  if (this.head !== null) {
+    toReturn = this.head.data;
+
+    if (this.tail === this.head) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+  }
+  this.size--;
+  return toReturn;
+};
+
+DoublyLinkedList.prototype.findStartingHead = function (value) {
+  let currentHead = this.head;
+  while (currentHead.next) {
+    if (currentHead.data == value) {
+      return true;
+    }
+    currentHead = currentHead.next;
+  }
+  return false;
+};
+
+DoublyLinkedList.prototype.findStartingTail = function (value) {
+  let currentTail = this.tail;
+  while (currentTail.prev) {
+    if (currentTail.data == value) {
+      return true;
+    }
+    currentTail = currentTail.prev;
+  }
+  return false;
+};
+
+function reverseSingleLinkedList(sll) {
+  let node = sll.head;
+  let prev = null;
+  while (node) {
+    let temp = node.next;
+    node.next = prev;
+    prev = node;
+    if (!temp) break;
+    node = temp;
+  }
+  return node;
+}
+function deleteDuplicateInUnsortedSllBest(sll1) {
+  let track = {};
+
+  let temp = sll1.head;
+  let prev = null;
+  while (temp) {
+    if (track[temp.data]) {
+      prev.next = temp.next;
+      sll1.size--;
+    } else {
+      track[temp.data] = true;
+      prev = temp;
+    }
+    temp = temp.next;
+  }
+  console.log(temp);
+}
+
 let dll1 = new DoublyLinkedList();
 dll1.insertAtHead(10); // ddl1's structure: tail: 10 head: 10
 dll1.insertAtHead(12); // ddl1's structure: tail: 10 head: 12
 dll1.insertAtHead(20); // ddl1's structure: tail: 10 head: 20
 dll1.insertAtTail(30); // ddl1's structure: tail: 30 head: 20
-console.log(dll1);
+dll1.insertAtTail(20); // ddl1's structure: tail: 20 head: 20
+deleteDuplicateInUnsortedSllBest(dll1);
+
